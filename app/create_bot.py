644701a9d161ee7eb4a88c977eee72f4c3427eb4
Sodx1 <anthonyprorok@hotmail.com>
2023-11-14 16:31:50 +0700

    work

--- app/create_bot.py
+++ app/create_bot.py
@@ -1,8 +1,13 @@
-import os 
+import os
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
 
-stroage = MemoryStorage()
-load_dotenv
+storage = MemoryStorage()
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+bot = Bot(token=TELEGRAM_TOKEN)
+dp = Dispatcher(bot, storage=storage)
---
+++ app/keyboards.py
@@ -0,0 +1,33 @@
+from aiogram.types import InlineKeyboardButton,InlineKeyboardMarkup,KeyboardButton,ReplyKeyboardMarkup
+
+multi_kb = InlineKeyboardMarkup(row_width=1)
+
+button_weather = InlineKeyboardButton(text='Узнать текущию погоду', 
+                                      callback_data="Weather")
+
+button_currency = InlineKeyboardButton(text='Конвектор валют', 
+                                      callback_data="Wallet")
+
+button_anime = InlineKeyboardButton(text='Отправь "Няшку"', 
+                                      callback_data="Anime")
+
+button_cancel = InlineKeyboardButton(text='Отмена',
+                                     callback_data='Back')
+
+(multi_kb.add(button_weather).add(button_currency)
+ .add(button_anime))
+multi_kb.add(button_cancel)
+
+
+# Создание основной клавиатуры
+button_funcs = KeyboardButton('Функционал')
+
+menu_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+menu_keyboard.add(button_funcs)
+
+# клавиатура повторить попытку/отмена
+else_or_cancel_kb = InlineKeyboardMarkup()
+repeat_button = InlineKeyboardButton(text='Повторить',
+                                     callback_data='Repeat')
+else_or_cancel_kb.add(button_cancel).add(repeat_button)
